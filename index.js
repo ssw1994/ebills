456b9af6935f9a31079ce505c8037f24fff957e9
sachin <sachin.waghmare@standardbank.co.za>
2022-07-16 23:47:20 +0530

    fix app creash issue

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use("/api", billRouter);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
 });
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("server started on port " + 5000);
+app.listen(PORT, () => {
+  console.log("server started on port " + PORT);
 });
